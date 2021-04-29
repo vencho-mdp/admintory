b8ed69470a69143f6294ea0bdef4cbdbee8501dc
Benicio Cardozo <beniciocardozomdp@gmail.com>
2021-04-29 20:26:55 -0300

    init git flow

--- api/controllers/suppliers.js
+++ api/controllers/suppliers.js
@@ -1,10 +1,78 @@
 const db = require('../db/db');
 
 class SupplierController {
-  async createSupplier(req, res) {}
-  async updateSupplier(req, res) {}
-  async deleteSupplier(req, res) {}
-  async getSupplier(req, res) {}
-  async getAllSuppliers(req, res) {}
+  async createSupplier(req, res) {
+    const {
+      full_name,
+      city,
+      address,
+      company,
+      phone_number,
+      userid
+    } = req.body;
+    try {
+      const [id] = await db('supplier')
+        .returning('id')
+        .insert({
+          full_name,
+          city,
+          address,
+          company,
+          phone_number,
+          userid
+        });
+      return res.status(201).json({
+        message: 'Supplier Created',
+        id
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Something went wrong',
+        error
+      });
+    }
+  }
+  async updateSupplier(req, res) {
+    const { id } = req.params;
+    try {
+      await db('supplier')
+        .update({
+          ...req.body
+        })
+        .where({ id });
+      return res.status(200).json({ message: 'Supplier Updated Successfully' });
+    } catch (error) {
+      return res.status(404).json({
+        message: 'Supplier does not exist'
+      });
+    }
+  }
+  async deleteSupplier(req, res) {
+    const { id } = req.params;
+    try {
+      await db('supplier')
+        .where({ id })
+        .del();
+      return res.status(202).json({
+        message: 'Supplier successfully eliminated'
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Something went wrong',
+        error
+      });
+    }
+  }
+  async getAllSuppliers(req, res) {
+    const { userid } = req.params;
+    try {
+      const suppliers = await db('supplier').where({ userid });
+      res.status(200).json([...suppliers]);
+    } catch (error) {
+      return res.status(404).json({
+        message: 'That user does not have any suppliers'
+      });
+    }
+  }
 }
-module.exports = new UserController();
+module.exports = new SupplierController();
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const auth = require('./auth');
+const supplier = require('./suppliers');
 
 const router = Router();
 
 router.use('/auth', auth);
+router.use('/suppliers', supplier);
 
 module.exports = router;
